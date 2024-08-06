405087ca43264f12c69d9e8e3ecb78f7736101e9
pioneeringdev <codedec116@gmail.com>
2024-08-06 15:14:47 -0500

    implement NthPrime

--- javascript/Sieve/sieve.js
+++ javascript/Sieve/sieve.js
@@ -1,6 +1,29 @@
 class Sieve {
   NthPrime(n) {
-    throw new Error("unimplemented");
+    if ( n === 0 || n === 1) {
+      return n + 2;
+    }
+    const primes = [2, 3];
+    let num = 3;
+    let squareNum;
+    let isPrime;
+    while(primes.length <= n) {
+      num += 2;
+      squareNum = Math.floor(Math.sqrt(num))
+      isPrime = true;
+      for(let i = 0; primes[i] <= squareNum && i <= primes.length; i++) {
+        if(num % primes[i] === 0) {
+          isPrime = false
+          break;
+        }
+      }
+
+      if(isPrime) {
+        primes.push(num);
+      }
+
+    }
+    return primes[primes.length - 1];
   }
 }
 
